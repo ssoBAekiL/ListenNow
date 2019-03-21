a7c7fecb5717a1443dcc9e00512d9fb9abba3735
cmiretf <37304718+cmiretf@users.noreply.github.com>
2019-03-21 18:16:49 +0100

    Add files via upload

---
+++ ListenNow/src/pruebas/pruebaPagoPremium.java
@@ -0,0 +1,18 @@
+package pruebas;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+
+import es.uam.eps.padsof.telecard.FailedInternetConnectionException;
+import es.uam.eps.padsof.telecard.InvalidCardNumberException;
+import es.uam.eps.padsof.telecard.OrderRejectedException;
+import es.uam.padsof.usuario.UsuarioRegistrado;
+
+public class pruebaPagoPremium {
+
+	public static void main(String[] args) throws IOException, InvalidCardNumberException, FailedInternetConnectionException, OrderRejectedException {
+
+	}
+
+}
